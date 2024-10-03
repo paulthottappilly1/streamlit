71d8bbc9e30b7f86978d91d7100a6d817d54a0ef
Paul Thottappilly <98353260+paulthottappilly1@users.noreply.github.com>
2024-10-03 05:25:30 +0000

    changes pt 2.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,6 @@ st.write("### The dataset used in the graphs below")
 df = pd.read_csv("Superstore_Sales_utf8.csv", parse_dates=True)
 st.dataframe(df)
 
-option = st.selectbox(
-    "The categories of the dataset ",
-    ("Furniture", "Office Supplies", "Technology"),
-    index=None,
-    placeholder="Select a category",
-)
-st.write("You selected:", option)
-
 # This bar chart will not have solid bars--but lines--because the detail data is being graphed independently
 st.bar_chart(df, x="Category", y="Sales")
 
@@ -39,7 +31,28 @@ st.line_chart(sales_by_month, y="Sales")
 
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
+option = st.selectbox(
+    "The categories of the dataset ",
+    ("Furniture", "Office Supplies", "Technology"),
+    index=None,
+    placeholder="Select a category",
+)
+st.write("You selected:", option)
+
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
+sub_categories_dict = {
+    "Furniture": ["Bookcases", "Chairs", "Furnishings", "Tables"], 
+    "Office Supplies": ["Appliances", "Art", "Binders", "Envelopes", "Fasteners", "Labels", "Paper", "Storage", "Supplies"], 
+    "Technology": ["Accessories", "Copiers", "Machines", "Phones"]
+}
+categories = list(sub_categories_dict.keys())
+selected_category = st.selectbox('Pick a category to view its corresponding sub_categories ', categories)
+if selected_category:
+    sub_categories = sub_categories_dict[selected_category]
+    selected_sub_categories = st.multiselect('Select the sub_categories', sub_categories)
+
+    st.write(f'You selected subcategories: {selected_sub_categories}')
+
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
